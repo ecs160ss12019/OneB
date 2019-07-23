c96ed55309416acb99a3434ae92f1a8a9d59284d
iceysteel <zainm211@hotmail.com>
2019-07-23 11:11:41 -0700

    fixed a logical error in the GameWaitingState class

--- app/src/main/java/com/example/breakout/States/GameWaitingState.java
+++ app/src/main/java/com/example/breakout/States/GameWaitingState.java
@@ -44,10 +44,11 @@ public class GameWaitingState extends State{
                 //also this part is probably completely wrong
                 //go to the next level in the level array here
                 Log.d("DEBUG", "going to level:" + gc.currentLevel);
-                if(gc.currentLevel != gc.levels.length) {
+                if(gc.currentLevel <= gc.levels.length) {
                     gc.context = gc.levels[gc.currentLevel - 1]; //new Level1State(gc); // go to running state
                 }
                 else{
+                    gc.currentLevel = 1;
                     gc.context = new Level1State(gc);
                 }
                 break;
