9a8d485e16120277b219b9a237170505a225dd9d
Michel Eter <micoror1@gmail.com>
2019-07-22 18:37:30 -0700

    Changed parameters for the menu

--- app/src/main/java/com/example/breakout/BOMenu.java
+++ app/src/main/java/com/example/breakout/BOMenu.java
@@ -9,12 +9,23 @@ public class BOMenu extends BOObject {
     int screenHeight;
     public float menuWidth;
     public float menuHeight;
+    public BOMenuButton resumeButton;
+    public BOMenuButton restartButton;
+    public BOMenuButton levelsButton;
+    public BOMenuButton leaderboardsButton;
+    public BOMenuButton exitButton;
 
-    BOMenu(int screenWidth, int screenHeight) {
+    BOMenu(int screenWidth, int screenHeight, BOGameController gc) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
         this.menuWidth = this.screenWidth / (float)1.75;
         this.menuHeight = this.screenHeight / (float)1.75;
+        resumeButton = new BOMenuButton(screenWidth, screenHeight, "Resume", gc);
+        levelsButton = new BOMenuButton(screenWidth, screenHeight, "Levels", gc);
+        restartButton = new BOMenuButton(screenWidth, screenHeight, "Restart", gc);
+        leaderboardsButton = new BOMenuButton(screenWidth, screenHeight, "Leaderboards", gc);
+        exitButton = new BOMenuButton(screenWidth, screenHeight, "Exit", gc);
+
 
         Log.d("MENU: ", ""+this.menuWidth);
         Log.d("MENU: ", ""+this.screenWidth);
