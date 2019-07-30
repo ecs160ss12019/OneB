d1e0c3750f4a402313524abb2d6e6ad695ec2ee7
Phillip Tran <pntran@ucdavis.edu>
2019-07-30 16:06:21 -0700

    Fixed Destroy Blocks

--- app/src/main/java/com/example/breakout/BOBlock.java
+++ app/src/main/java/com/example/breakout/BOBlock.java
@@ -47,7 +47,8 @@ public class BOBlock extends BOObject{
             isDead = true;
             // check if the block hit has a power-up
             if (hasPowerUp) {
-                gc.powerUp = gc.powerUp.randomPowerUp(gc);
+                //gc.powerUp = gc.powerUp.randomPowerUp(gc);
+                gc.powerUp = new BODestroyBlocks(gc);
                 Log.d("" + gc.powerUp + " PowerUp", "Activated");
             }
             gc.score += 10;
--- app/src/main/java/com/example/breakout/BODestroyBlocks.java
+++ app/src/main/java/com/example/breakout/BODestroyBlocks.java
@@ -1,26 +1,48 @@
 package com.example.breakout;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class BODestroyBlocks extends BOPowerUp {
 
     public BOGameController gc;
+    ArrayList<Integer> activeBlocks = new ArrayList<>();
 
     public BODestroyBlocks(BOGameController gc) {
         this.gc = gc;
+        for (int i = 0; i < gc.blocks.size(); i++){
+            if(gc.blocks.get(i).getDeadStatus() == false)
+                activeBlocks.add(i);
+        }
+
+
+        int blocksToDestroy = activeBlocks.size() / 2;
+
+        Random random = new Random();
+        while(blocksToDestroy > 0) {
+
+            int position = random.nextInt(activeBlocks.size());
+            int blockToDestroy = activeBlocks.get(position);
+            //it can select same block twice.
+            gc.blocks.get(blockToDestroy).setDeadStatus();
+            gc.blocks.get(blockToDestroy).collider = new RectF(-1,-1,-1,-1);
+            blocksToDestroy--;
+        }
+
+
+    }
+
+    public void draw(Canvas mCanvas, Paint mPaint)
+    {
+        //mCanvas.drawText("Bomb!");
     }
 
     // Code the logic of our power-up.
     public void apply(BOGameController gc) {
-            int blocksAlive = 0;
-            for (int i = 0; i < gc.blocks.size(); i++){
-                if (!gc.blocks.get(i).getDeadStatus()) {
-                    blocksAlive++;
-                }
-            }
-            for (int j = 0; j < blocksAlive % 2; j++) {
-                if (!gc.blocks.get(j).getDeadStatus()) {
-                    gc.blocks.get(j).setDeadStatus();
-                }
-            }
+
     }
 
     public String type() {
