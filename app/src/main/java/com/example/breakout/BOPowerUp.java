bc20e38578342f7ce5f9cf3cfc4f0bde11b3414c
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2019-07-30 14:16:10 -0700

    Destroy Blocks Powerup Enabled, But Not Currently Working.

--- app/src/main/java/com/example/breakout/BOPowerUp.java
+++ app/src/main/java/com/example/breakout/BOPowerUp.java
@@ -11,7 +11,7 @@ public abstract class BOPowerUp {
 
     public BOPowerUp randomPowerUp(BOGameController gc) {
         Random random = new Random();
-        int choice = random.nextInt(3);
+        int choice = random.nextInt(4);
 
         if(choice == 0) {
             return(new BODoubleBall(gc));
@@ -21,6 +21,9 @@ public abstract class BOPowerUp {
         }
         else if(choice == 2) {
             return(new BOExtraLife(gc));
+        }
+        else if(choice == 3){
+            return(new BODestroyBlocks(gc));
         }
 
         return null; // crash
